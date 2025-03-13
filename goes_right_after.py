21c9ea62baee596b9b5890b47b44014204923ad8
GregNault <gnault@asu.edu>
2025-03-13 04:18:34 -0400

    Completed

--- goes_right_after.py
+++ goes_right_after.py
@@ -1,16 +1,18 @@
+# Returns true if in the sting there are two characters where the first character preceeds the second.
 def goes_after(word: str, first: str, second: str) -> bool:
     index_list = []
     if word.count(first) == 0:
         return False
-    if word.endswith(first) == True:
-        return False
-    
     ind1 = word.index(first)
-    if word[ind1 + 1] == second:
-        return True
+    try:
+        if word[ind1 + 1] == second:
+            return True
+    except:
+        return False
     return False
 
 
 print("Example:")
 print(goes_after("world", "w", "o"))
-print(goes_after("panorama", "a", "n"))
+print(goes_after("plnorlma", "a", "n"))
+print(goes_after("Almaz", "m", "a"))
